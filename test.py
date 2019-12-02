7942a316a14f69722d772465ae1470e72ca54640
Andrew Chen <achen88@mit.edu>
2019-12-02 15:02:34 -0500

    added test accuracy score

--- test.py
+++ test.py
@@ -2,5 +2,11 @@ from searcher import Searcher
 
 f = open('corpus.txt', 'r')
 searcher = Searcher()
+total = 0
+failures = 0
 for line in f:
-  print(line, searcher.search(line[:-1]))
+  total += 1
+  res = searcher.search(line[:-1])
+  if res == '** could not find song': failures += 1
+  print(line, res)
+print('score: ' + str(total-failures) + '/' + str(total))
